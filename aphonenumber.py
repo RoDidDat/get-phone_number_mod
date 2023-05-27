5f2c277980d90bba77ccb275330e4cda9173b28f
RoDidDat <59644909+RoDidDat@users.noreply.github.com>
2023-05-27 08:12:35 +0100

    Add files via upload

---
+++ aphonenumber.py
@@ -0,0 +1,17 @@
+import get_phone_number as gpn
+
+
+
+
+numberrr = 'USE A PHONE NUMBER HERE'
+#use a phone number here
+
+
+
+
+# use the functions from the get_phone_number module along
+# with the numberrr variable
+gpn.Origin_Of_Phone_Num(numberrr)
+gpn.Service_Provider_Of_Num(numberrr)
+gpn.Get_Location_Of_Phone_Number(numberrr)
+
---
+++ get_phone_number.py
@@ -0,0 +1,61 @@
+'''A Module For Grabbing Information On Phone Numbers (THIS INCLUDE MODULES: phonenumbers; opencage; folium)'''
+
+import phonenumbers
+import opencage
+import folium
+
+from phonenumbers import geocoder as gc
+
+
+
+
+def Origin_Of_Phone_Num(FoneNumber= int):
+    """Print Out The Name Of Origin Of The Phone Number"""
+    
+    the_phone_num = phonenumbers.parse(FoneNumber)
+    location = gc.description_for_number(the_phone_num, 'en')
+    print(location)
+
+
+
+def Service_Provider_Of_Num(FoneNumber= int):
+    '''Print Out The Service Provider Of The Phone Number'''
+    
+    from phonenumbers import carrier
+    service_pro = phonenumbers.parse(FoneNumber)
+    print(carrier.name_for_number(service_pro, 'en'))
+
+
+
+def Get_Location_Of_Phone_Number(FoneNumber= int):
+    '''Get The Location Of The Phone Number. 
+    !!! I WILL NOT BE INCLUDING MY OPENCAGE API KEY, 
+    YOU CAN GET IT YOUR SELF BY MAKING AN ACCOUNT FOR FREE ONLINE!!!'''
+
+    the_phonenum = phonenumbers.parse(FoneNumber)
+    location = gc.description_for_number(the_phonenum, 'en')
+
+    #print out the location of the phone number
+    from opencage.geocoder import OpenCageGeocode
+    key = 'GET A OPENCAGE KEY'
+    #use you opencage account to to copy your api key code into this variable
+    geocoder = OpenCageGeocode(key)
+    query = str(location)
+    results = geocoder.geocode(query)
+    print(results)
+
+    #get the longnitude and latitude
+    lat = results[0]['geometry']['lat']
+    lng = results[0]['geometry']['lng']
+    print(lat, lng)
+
+    #save the map location
+    myMapp = folium.Map(location=[lat, lng], zoom_start=9)
+    folium.Marker([lat, lng], popup=location).add_to(myMapp)
+
+    myMapp.save('Printed_Location.html')
+    
+    
+
+
+
